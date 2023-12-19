c367a4f2ffc4db88c747522ecff932bab0500eec
MRG_341\mangl <manglada05305@gmail.com>
2023-12-19 10:01:30 +0100

    Update CUsers

--- QUERYsCreatingUsr.sql
+++ QUERYsCreatingUsr.sql
@@ -1,9 +1,25 @@
-INSERT INTO Users (id, username, password_hash, name, lastname, birthdate, phone_number, postal_address, mail, ssss_num, company_position, description)
-VALUES (1, 'Mrg_341', crypt('Movistar24#', gen_salt('bf')), 'Miguel', 'Anglada_Jover', '2005-03-03', '610164688', '07008', 'manglada05305@gmail.com', '0000', 'Software_Dev', 'Me estoy tomando un café con Nesquik')
+-- Si es necesario hacer alguna de las siguientes tareas,
+-- copiar el trocito de código necesario.
+-- NO REALIZAR MODIFICACIONES EN LO QUE ESTÁ ESCRITO.
+
+-- Área de modificaciones Init
+
+-- Área de modificaciones End
+
+-- User add Syntax Init
+INSERT INTO Users (id, username, password_hash, name, lastname, birthdate, phone_number, postal_address, mail, ssss_num, company_position, description, hour_count)
+VALUES (, '', crypt('', gen_salt('bf')), '', '', 'XXXX-YY-ZZ', '', '', '', '', '', '', '0')
+-- User add Syntax End
 
 SELECT * FROM Users
 
-UPDATE Users WHERE username 'Mrg_341' INSERT INTO Hour_Count VALUE '5'
+-- Update a value syntax Init
+UPDATE {{Table}}
+SET {{Column}}= ''
+WHERE username = '';
+-- Update a value syntax End
 
-ALTER TABLE Users
-ADD COLUMN Hour_Count INT
+
+
+
+
