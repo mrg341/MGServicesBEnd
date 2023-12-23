5fe1bfb63e64f82c116aec723e4798f9651bfce7
MRG_341\mangl <manglada05305@gmail.com>
2023-12-23 13:27:29 +0100

    Update

--- QUERYsCreatingHours.sql
+++ QUERYsCreatingHours.sql
@@ -6,3 +6,13 @@ CREATE TABLE users_working_hours (
 );
 
 INSERT INTO users_working_hours (id, user_id, hours_left, hours_worked)
+VALUES ('1', 1, 0, 20)
+
+SELECT *
+FROM users_data
+JOIN users_working_hours ON USERS_DATA.ID = USERS_WORKING_HOURS.user_id
+
+SELECT USERS_DATA.ID, USERS_DATA.NAME, USERS_DATA.LASTNAME, users_working_hours.hours_left, users_working_hours.hours_worked
+FROM USERS_DATA
+JOIN users_working_hours ON USERS_DATA.ID = users_working_hours.user_id
+WHERE USERS_DATA.ID = 1;
